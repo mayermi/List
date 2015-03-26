7654bbc99c09773a40cbb35f8c12346a3cc51772
Miriam Mayer <miri.mayer@t-online.de>
2015-03-26 13:08:38 +0100

    reminders work, or do not work. that is the question. I should learn ttd

--- JS/event.js
+++ JS/event.js
@@ -7,6 +7,8 @@
     	var eventid = query.split("=")[1];
 
 		var element = localStorage.getItem('ID=' + eventid).split(',');
+
+    checkAnniversary();
 		
         var picturedata = localStorage.getItem('imgID=' + eventid);
     if (picturedata) {
@@ -33,12 +35,119 @@
         var rlatitude = Math.round(latitude * 1000) / 1000;
         
         if(rsavedlongitude === rlongitude && rsavedlatitude === rlatitude) {
-          alert('You are near ' + savedelement[0] + '!');
+          checkLastReminder(maximumID);
         }
         
         maximumID++;
       }
       maximumID = 1;
+    }
+
+    function checkAnniversary() {
+      while (!(localStorage.getItem('ID=' + maximumID) === null)) {
+          var element = localStorage.getItem('ID=' + maximumID).split(',');
+          if (element[6] === 'year') {
+            if (getDate() === element[8] && getTime() === element[4]) {
+              alert('You are near ' + savedelement[0] + '!');
+
+              // save when last reminded
+              localStorage.setItem('ID=' + maximumID, [
+                element[0], element[1], element[2], element[3], element[4], element[5], element[6], element[7], 
+                getDate()]);
+            }
+          } else if (element[6] === 'six') {
+            getDateMonstLater(6, element);
+          } else if (element[6] === 'three') {
+            getDateMonstLater(3, element);
+          } else if (element[6] === 'one') {
+            getDateMonstLater(1, element);
+          } else if (element[6] === 'never') {
+          }
+          maximumID++;
+        }
+        maximumID = 1;
+    }
+
+    function checkLastReminder(ID) {
+      var element = localStorage.getItem('ID=' + ID).split(',');
+      if (element[7] === 'year') {
+        if (element[8].split("/").pop() < getDate().split("/").pop()) {
+          alert('More than a year ago ' + savedelement[0] + ' happend!');
+
+          // save when last reminded
+          localStorage.setItem('ID=' + maximumID, [
+            element[0], element[1], element[2], element[3], element[4], element[5], element[6], element[7], 
+            getDate()]);
+        }
+        
+      } else if (element[7] === 'six') {
+        checkBiggerDate(6, element);
+      } else if (element[7] === 'three') {
+        checkBiggerDate(3, element);
+      } else if (element[7] === 'one') {
+        checkBiggerDate(1, element);
+      } else if (element[7] === 'never') {
+      }
+    }
+
+    function getDateMonstLater(numberOfMonthsLater, element) {
+      var start_pos = element[8].indexOf('/') + 1;
+      var end_pos = element[8].indexOf('/',start_pos);
+      var monthXMonsthsLater = parseInt(element[8].substring(start_pos,end_pos)) + numberOfMonthsLater;
+      if (monthXMonsthsLater > 12) {
+        monthXMonsthsLater = monthXMonsthsLater - 12;
+      }
+      var dateXMonsthsLater = element[8].split("/").shift() + '/0' + monthXMonsthsLater +  "/" + element[8].split("/").pop();
+      if (getDate() === dateXMonsthsLater && getTime() === element[4]) {
+        alert(numberOfMonthsLater + 'months ago ' + savedelement[0] + ' happend!');
+
+        // save when last reminded
+        localStorage.setItem('ID=' + maximumID, [
+          element[0], element[1], element[2], element[3], element[4], element[5], element[6], element[7], 
+          getDate()]);
+      }
+    }
+
+    function checkBiggerDate(numberOfMonthsLater, element) {
+      if (element[8].split("/").pop() === getDate().split("/").pop()) {
+        var start_pos = element[8].indexOf('/') + 1;
+        var end_pos = element[8].indexOf('/',start_pos);
+        var reminderMonth = parseInt(element[8].substring(start_pos,end_pos));
+
+        var start_pos_now = getDate().indexOf('/') + 1;
+        var end_pos_now = getDate().indexOf('/',start_pos);
+        var nowMonth = parseInt(element[8].substring(start_pos_now,end_pos_now));
+
+        if (reminderMonth + numberOfMonthsLater < nowMonth) {
+          alert('More than ' + numberOfMonthsLater + 'months ago ' + savedelement[0] + ' happend!');
+
+          // save when last reminded
+          localStorage.setItem('ID=' + maximumID, [
+            element[0], element[1], element[2], element[3], element[4], element[5], element[6], element[7], 
+            getDate()]);
+        }
+      } else if (element[8].split("/").pop() < getDate().split("/").pop()) {
+        var start_pos = element[8].indexOf('/') + 1;
+        var end_pos = element[8].indexOf('/',start_pos);
+        var reminderMonth = parseInt(element[8].substring(start_pos,end_pos));
+
+        var start_pos_now = getDate().indexOf('/') + 1;
+        var end_pos_now = getDate().indexOf('/',start_pos);
+        var nowMonth = parseInt(element[8].substring(start_pos_now,end_pos_now));
+
+        if (element[8].split("/").pop() + 1 === getDate().split("/").pop()) {
+          if (reminderMonth + numberOfMonthsLater > 12) {
+            if (reminderMonth + numberOfMonthsLater - 12 < nowMonth) {
+              alert('More than ' + numberOfMonthsLater + 'months ago ' + savedelement[0] + ' happend!');
+
+              // save when last reminded
+              localStorage.setItem('ID=' + maximumID, [
+                element[0], element[1], element[2], element[3], element[4], element[5], element[6], element[7], 
+                getDate()]);
+            }
+          }
+        }
+      }
     }
 
 		var map;
@@ -91,5 +200,24 @@
 		}
 
 		google.maps.event.addDomListener(window, 'load', initialize);
+
+
+    function getDate() {
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth()+1;
+      var yyyy = today.getFullYear();
+
+      if(dd<10) {
+        dd='0'+dd
+      } 
+
+      if(mm<10) {
+        mm='0'+mm
+      } 
+
+      today = dd+'/'+mm+'/'+yyyy;
+      return today;
+    }
 	});
 })();
